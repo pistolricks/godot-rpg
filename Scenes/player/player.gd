880772d283346b88b65fe2441e114012daf78c1b
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-08-06 12:40:09 -0700

    Add initial project setup with core files
    - Configured `.editorconfig` and `.gitattributes` for consistent formatting and EOL handling.
    - Added `.gitignore` for Godot-specific and editor-specific files.
    - Created basic Godot project structure including `player` and `world` scenes.
    - Implemented basic player movement logic in `player.gd`.

---
+++ Scenes/player/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed:int = 80
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+
+	velocity = input_vector*speed
+	move_and_slide()
