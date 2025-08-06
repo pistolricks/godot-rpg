f232d22c305b8597683a38b6517d3300481e87c6
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-08-06 13:32:44 -0700

    Add melee attack functionality and weapon holder to player
    - Integrated `weapon_holder` with a melee attack system in `player.tscn`.
    - Updated `player.gd` for weapon interactions and added basic damage logic.
    - Adjusted collision shapes and added new sprites for melee attacks.

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -1,11 +1,28 @@
 extends CharacterBody2D
 
+@onready var weapon_marker: Node = get_node("weapon_holder")
 @export var speed:int = 80
 
-func _physics_process(delta: float) -> void:
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+var dying:bool = false
+var attacking_melee:bool = false 
+var attacking_ranged:bool = false
 
-	velocity = input_vector*speed
-	move_and_slide()
+
+var health: int = 5
+var damage: int = 2
+
+func _physics_process(delta: float) -> void:
+	
+	if not dying:
+		weapon_marker.look_at()
+		var input_vector: Vector2 = Vector2.ZERO
+		input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+		velocity = input_vector*speed
+		move_and_slide()
+
+
+func _on_melee_body_entered(body: Node2D) -> void:
+	# Check for damage from enemy
+	pass 
