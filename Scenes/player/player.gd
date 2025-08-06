56afd6a6f70820044b4594ab0f33635b2a4ef125
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-08-06 13:38:35 -0700

    Refactor player.gd by extracting attack_check function and integrating it in _physics_process

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -15,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	
 	if not dying:
 		weapon_marker.look_at(get_global_mouse_position())
+		attack_check(delta)
 		var input_vector: Vector2 = Vector2.ZERO
 		input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 		input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -26,3 +27,6 @@ func _physics_process(delta: float) -> void:
 func _on_melee_body_entered(body: Node2D) -> void:
 	# Check for damage from enemy
 	pass 
+
+func attack_check(delta: float):
+	pass
